ed3fcfecbd41bcdf5ff2a5f54409757343684850
Dmitrii Romanenco <dmitrii.romanenco@amdaris.com>
2022-09-01 20:10:58 +0300

    Add project files.

---
+++ Hometask 2.7/Program.cs
@@ -0,0 +1,19 @@
+﻿// Hometask 2.7
+
+// 1. Configure Entity Framework Core.
+// 2. Create repository projects.
+// 3. Map Entity in the app.
+
+
+using System;
+
+namespace App
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
